624f736294e56cf3207aa97fa10bd989705bdadb
ecoyle2019 <ethan.coyle@yale.edu>
2022-12-10 16:21:38 -0500

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -1,12 +1,22 @@
-#Tile Class
+class Tile:
 
-class Tile():
     MAX_HEIGHT = 4
+
     def __init__(self):
-        self.height = 0
-        #self.worker = None
-    
-    def build(self):
-        if (self.height >= 4):
-            #raise error
-        self.height = self.height + 1
+        self.height = '0'
+        self.row = None # this is added upon creation
+        self.column = None # added upon creation
+        self.worker = None
+
+
+    # attributes:
+        # level built
+        # worker occupying the space
+
+    # methods:
+        # 
+
+
+# maybe use Factory pattern to create the tiles -- not sure
+# State pattern for whether the tile is occupied and/or has an L4 building
+    # state will be marked as free if there is no worker on it and no L4 building
