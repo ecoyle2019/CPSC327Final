b24d0dee05a10d1b9a9fb24b86a4e0e38ede4d69
maxvelasco <50286542+maxvelasco@users.noreply.github.com>
2022-12-13 12:40:17 -0500

    Update heuristic.py

--- heuristic.py
+++ heuristic.py
@@ -71,7 +71,7 @@ class Heuristic():
         # worker2_d2 = max( abs(player_workers[1].row - opponent_workers[1].row), abs(player_workers[1].col - opponent_workers[1].col))
 
         # d1 = min(worker1_d1, worker2_d1)
-        # d2 = min(worker2_d1, worker2_d2)
+        # d2 = min(worker1_d2, worker2_d2)
         # distance_score = 8 - (d1 + d2)
 
         # return distance_score
